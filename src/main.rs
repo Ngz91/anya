763b43bb1cc0c022b0a3a981c19afe0072e3ba14
Nevio <neviogomez91@gmail.com>
2023-11-06 17:21:31 -0400

    Move logic to App struct

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,76 @@ use crossterm::{
     ExecutableCommand,
 };
 use ratatui::{
-    prelude::{Constraint, CrosstermBackend, Direction, Layout, Terminal, Rect},
+    prelude::{Constraint, CrosstermBackend, Direction, Layout, Rect, Terminal},
     style::{Color, Style, Stylize},
     widgets::{Block, Borders, Padding, Paragraph, Wrap},
     Frame,
 };
-use std::rc;
-use std::io::{stdout, Result};
+use std::{
+    io::{stdout, Result},
+    rc::Rc,
+};
+
+struct App<'a> {
+    request: Option<&'a str>,
+    response: Option<&'a str>,
+}
+
+impl<'a> App<'a> {
+    fn new(request: Option<&'a str>, response: Option<&'a str>) -> Self {
+        Self { request, response }
+    }
+
+    fn render_ui(&self, f: &mut Frame) {
+        let main_layout = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+            .split(f.size());
+        let request_layout = Layout::default()
+            .direction(Direction::Vertical)
+            .horizontal_margin(1)
+            .constraints([Constraint::Percentage(7), Constraint::Percentage(93)])
+            .split(main_layout[0]);
+        let response_layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Percentage(100)])
+            .split(main_layout[1]);
+
+        f.render_widget(
+            Block::default()
+                .borders(Borders::all())
+                .blue()
+                .title("Request Url"),
+            request_layout[0],
+        );
+        f.render_widget(
+            Block::default()
+                .borders(Borders::all())
+                .blue()
+                .title("Json"),
+            request_layout[1],
+        );
+        f.render_widget(
+            Block::default()
+                .borders(Borders::all())
+                .green()
+                .title("Response"),
+            response_layout[0],
+        );
+    }
+
+    #[tokio::main]
+    async fn test_get_client(&self) -> std::result::Result<std::string::String, reqwest::Error> {
+        let client = reqwest::Client::new();
+        let resp = client
+            .get("https://httpbin.org/get")
+            .send()
+            .await?
+            .json::<serde_json::Value>()
+            .await?;
+        Ok(resp.to_string())
+    }
+}
 
 fn create_text(text: &str, padding: Vec<u16>) -> Paragraph<'_> {
     Paragraph::new(text)
@@ -27,72 +90,16 @@ fn create_text(text: &str, padding: Vec<u16>) -> Paragraph<'_> {
         .wrap(Wrap { trim: true })
 }
 
-fn ui(f: &mut Frame) {
-    let main_layout = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
-        .split(f.size());
-    let request_layout = Layout::default()
-        .direction(Direction::Vertical)
-        .horizontal_margin(1)
-        .constraints([Constraint::Percentage(7), Constraint::Percentage(93)])
-        .split(main_layout[0]);
-    let response_layout = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([Constraint::Percentage(100)])
-        .split(main_layout[1]);
-
-    f.render_widget(
-        Block::default()
-            .borders(Borders::all())
-            .blue()
-            .title("Request Url"),
-        request_layout[0],
-    );
-    f.render_widget(
-        Block::default()
-            .borders(Borders::all())
-            .blue()
-            .title("Json"),
-        request_layout[1],
-    );
-    f.render_widget(
-        Block::default()
-            .borders(Borders::all())
-            .green()
-            .title("Response"),
-        response_layout[0],
-    );
-}
-
-fn render_response_text(f: &mut Frame, area: rc::Rc<[Rect]>) {
-    let test_response = test_get_client().unwrap();
-    let binding = test_response.to_string();
-
-    let r = create_text(&binding, vec![2, 2, 1, 2]);
-    f.render_widget(r, area[0])
-}
-
-#[tokio::main]
-async fn test_get_client() -> std::result::Result<serde_json::Value, reqwest::Error> {
-    let client = reqwest::Client::new();
-    let resp = client
-        .get("https://httpbin.org/get")
-        .send()
-        .await?
-        .json::<serde_json::Value>()
-        .await?;
-    Ok(resp)
-}
-
 fn main() -> Result<()> {
     stdout().execute(EnterAlternateScreen)?;
     enable_raw_mode()?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
     terminal.clear()?;
 
+    let app = App::new(None, None);
+
     loop {
-        terminal.draw(ui)?;
+        terminal.draw(|f| app.render_ui(f))?;
         if event::poll(std::time::Duration::from_millis(16))? {
             if let event::Event::Key(key) = event::read()? {
                 if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
