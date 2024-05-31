5711c1a7e02acdf6650895b8bd4c11517ba7d4f9
Nevio <neviogomez91@gmail.com>
2024-05-31 17:42:52 -0400

    Adjust code from merge with main (input processing)

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,7 @@
 use std::{io, time::Duration};
 
 use arboard::Clipboard;
+use crossterm::event::{self, Event};
 use ratatui::{
     backend::Backend,
     style::Stylize,
@@ -10,15 +11,36 @@ use ratatui::{
 use tokio::sync::{mpsc, watch};
 use tui_textarea::{Input, Key, TextArea};
 
-use crate::errors;
-use crate::utils;
-use crate::MainLayout;
+use crate::{errors, utils, MainLayout};
+
+type RequestResult = std::result::Result<serde_json::Value, errors::CustomError>;
+
+async fn make_request(
+    client_builder: Result<reqwest::RequestBuilder, errors::CustomError>,
+) -> std::result::Result<serde_json::Value, errors::CustomError> {
+    match client_builder {
+        Ok(client) => {
+            let resp = client.send().await?.json::<serde_json::Value>().await?;
+            Ok(resp)
+        }
+        Err(err) => Err(err),
+    }
+}
+
+#[derive(Default, Clone, Debug, PartialEq, Eq)]
+pub enum State {
+    #[default]
+    Idle,
+    Running,
+    Exit,
+}
 
 #[derive(Default)]
 pub struct App<'a> {
     response: Option<serde_json::Value>,
     textarea: [TextArea<'a>; 2],
     which: usize,
+    state: State,
 }
 
 impl App<'_> {
@@ -36,67 +58,84 @@ impl App<'_> {
         response_rx: mpsc::UnboundedReceiver<Result<serde_json::Value, errors::CustomError>>,
     ) -> io::Result<()> {
         self.textarea[0].insert_str("https://");
+        let tick_rate = Duration::from_millis(250);
 
         loop {
             terminal.draw(|f| {
                 let layout = MainLayout::new(f);
                 self.render_ui(f, &layout);
             })?;
+            if crossterm::event::poll(tick_rate)? {
+                if let Event::Key(key_event) = event::read().unwrap() {
+                    let input = key_event.into();
 
-            match crossterm::event::read()?.into() {
-                Input { key: Key::Esc, .. }
-                | Input {
-                    key: Key::Char('q'),
-                    ctrl: true,
-                    ..
-                } => {
-                    let _ = cancel_send.send(true);
-                    return Ok(());
-                }
-                Input {
-                    key: Key::Char('x'),
-                    ctrl: true,
-                    ..
-                } => {
-                    self.change_textarea();
-                }
-                // GET method
-                Input {
-                    key: Key::Char('g'),
-                    ctrl: true,
-                    ..
-                } => {
-                    let resp = self.request(client, reqwest::Method::GET).await;
-                    self.set_response(resp)
-                }
-                // POST method
-                Input {
-                    key: Key::Char('h'),
-                    ctrl: true,
-                    ..
-                } => {
-                    let resp = self.request(client, reqwest::Method::POST).await;
-                    self.set_response(resp)
-                }
-                // Paste clipboard contents into the active textarea
-                Input {
-                    key: Key::Char('l'),
-                    ctrl: true,
-                    ..
-                } => {
-                    let clip_text = clipboard.get_text().unwrap();
-                    self.textarea[self.which].insert_str(clip_text);
-                }
-                // Select textarea contents
-                Input {
-                    key: Key::Char('k'),
-                    ctrl: true,
-                    ..
-                } => {
-                    self.textarea[self.which].select_all();
-                }
-                input => {
-                    self.handle_inputs(input);
+                    match input {
+                        Input { key: Key::Esc, .. }
+                        | Input {
+                            key: Key::Char('q'),
+                            ctrl: true,
+                            ..
+                        } => {
+                            let _ = cancel_send.send(true);
+                            self.state = State::Exit
+                        }
+
+                        Input {
+                            key: Key::Char('x'),
+                            ctrl: true,
+                            ..
+                        } => {
+                            self.change_textarea();
+                        }
+                        // GET method
+                        Input {
+                            key: Key::Char('g'),
+                            ctrl: true,
+                            ..
+                        } => {
+                            let client_builder = self.build_client(client, reqwest::Method::GET);
+                            tokio::select! {
+                                response = make_request(client_builder) => {
+                                    self.set_response(response)
+                                }
+                            }
+                        }
+                        // POST method
+                        Input {
+                            key: Key::Char('h'),
+                            ctrl: true,
+                            ..
+                        } => {
+                            let client_builder = self.build_client(client, reqwest::Method::POST);
+                            tokio::select! {
+                                response = make_request(client_builder) => {
+                                    self.set_response(response)
+                                }
+                            }
+                        }
+                        // Paste clipboard contents into the active textarea
+                        Input {
+                            key: Key::Char('l'),
+                            ctrl: true,
+                            ..
+                        } => {
+                            let clip_text = clipboard.get_text().unwrap();
+                            self.textarea[self.which].insert_str(clip_text);
+                        }
+                        // Select textarea contents
+                        Input {
+                            key: Key::Char('k'),
+                            ctrl: true,
+                            ..
+                        } => {
+                            self.textarea[self.which].select_all();
+                        }
+                        _ => self.render_inputs(input),
+                    }
+
+                    if self.state == State::Exit {
+                        return Ok(());
+                    }
                 }
             }
         }
@@ -139,11 +178,11 @@ impl App<'_> {
         utils::deactivate(&mut self.textarea[1]);
     }
 
-    async fn request(
+    fn build_client(
         &mut self,
         client: &reqwest::Client,
         method: reqwest::Method,
-    ) -> std::result::Result<serde_json::Value, errors::CustomError> {
+    ) -> Result<reqwest::RequestBuilder, errors::CustomError> {
         let request_url = &self.textarea[0].lines()[0];
 
         let has_json = !self.textarea[1].lines()[0].is_empty();
@@ -166,19 +205,10 @@ impl App<'_> {
             false => {} // Do nothing, no JSON
         }
 
-        let resp = request_builder
-            .send()
-            .await?
-            .json::<serde_json::Value>()
-            .await?;
-
-        Ok(resp)
+        Ok(request_builder)
     }
 
-    fn set_response(
-        &mut self,
-        response: std::result::Result<serde_json::Value, errors::CustomError>,
-    ) {
+    fn set_response(&mut self, response: RequestResult) {
         self.response = match response {
             Ok(resp) => Some(resp),
             Err(err) => Some(serde_json::json!({
@@ -193,7 +223,7 @@ impl App<'_> {
         utils::activate(&mut self.textarea[self.which])
     }
 
-    fn handle_inputs(&mut self, input: Input) {
+    fn render_inputs(&mut self, input: Input) {
         self.textarea[self.which].input(input);
     }
 }
